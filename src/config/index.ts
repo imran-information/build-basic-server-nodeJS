2e5ce40020c89ebd6d67d25f9e92f76ea7dedefc
Imran Hossain <imranh.dev1@gmail.com>
2025-11-30 02:53:38 +0600

    post a data use postman and checking...

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,9 @@ import path from "path";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
+const config = {
+    env: process.env.NODE_ENV,
+    port: process.env.PORT
+}
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,56 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.end(
-        JSON.stringify({
-            message: "Hello from node js with typescript...",
-            path: req.url,
-        }
+    console.log('server is running....');
+    // get end point
+    if (req.url === "/" && req.method === "GET") {
+        res.writeHead(200, { 'content-type': "application/json" });
+        res.end(
+            JSON.stringify({
+                message: "Hello from node js with typescript...",
+                path: req.url,
+            })
         )
-    )
+    }
+    // api route
+    if (req.url === "/api" && req.method === "GET") {
+        res.writeHead(200, { 'content-type': "application/json" });
+        res.end(
+            JSON.stringify({
+                message: "Health status ok...",
+                path: req.url,
+            })
+        )
+    }
+
+    // users route
+    if (req.url === "/api/users" && req.method === "POST") {
+        // const user = {
+        //     id: 1,
+        //     name: 'imran'
+        // }
+        // res.writeHead(200, { 'content-type': "application/json" });
+        // res.end(
+        //     JSON.stringify(user)
+        // )
+        let bodyUserData = '';
+
+        req.on("data", (chunk) => {
+            bodyUserData += chunk.toString();
+        });
+
+        req.on("end", () => {
+            const parsBodyData = JSON.parse(bodyUserData)
+            console.log(parsBodyData);
+            res.end(JSON.stringify(parsBodyData));
+        })
+
+    }
 })
 
-server.listen(5000, () => {
-    console.log(`server is running on port ${5000}`);
+
+
+server.listen(config.port, () => {
+    console.log(`server is running on port ${config.port}`);
 })
